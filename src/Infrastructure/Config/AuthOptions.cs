27f528285e44d139800c72fed9f475dc1d3a38b6
UlanZhurymbay <zhurimbay@gmail.com>
2022-09-26 12:53:38 +0600

    #2 Создал класс для того чтобы реализовать JWT

---
+++ src/Infrastructure/Config/AuthOptions.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Config
+{
+    public static class AuthOptions
+    {
+        private const int LifeTimeTokenInMinute = 20;
+        public const int LifeTimeRefreshTokenInYear = 1;
+        private const string Issuer = "DeliveryApp";
+        private const string Audience = "AuthClient";
+
+
+        public static TokenValidationParameters ValidationParameters(byte[] key, bool validateLifetime)
+        {
+            return  new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = Issuer,
+                ValidateAudience = true,
+                ValidAudience = Audience,      
+                ValidateLifetime = validateLifetime,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero
+            };
+        }
+
+        public static JwtSecurityToken SecurityToken(List<Claim> claims, byte[] key)
+        {
+            var now = DateTime.UtcNow;
+            return new JwtSecurityToken(
+                Issuer,
+                Audience,
+                notBefore: now,
+                claims: claims,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256),
+                expires: now.Add(TimeSpan.FromMinutes(LifeTimeTokenInMinute)));
+        }
+    }
+}
