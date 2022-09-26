c859329259f02c4c97cffc74668f737b343ad56c
UlanZhurymbay <zhurimbay@gmail.com>
2022-09-26 13:03:01 +0600

    #2 Создал класс для того чтобы работать с токенами в сервисах

---
+++ src/ApplicationCore/Entities/ApiEntities/RefreshTokenInfo.cs
@@ -0,0 +1,8 @@
+namespace ApplicationCore.Entities.ApiEntities
+{
+    public class RefreshTokenInfo
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
