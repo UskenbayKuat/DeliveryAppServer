ffadf59d114c270ebb254059b48ae418026aa4c7
Yerdaulet Bulatov <yerdaulet.bulatov@gmail.com>
2022-09-29 01:42:52 +0600

    #4 Обновлены поля Car

--- src/ApplicationCore/Entities/AppEntities/Car.cs
+++ src/ApplicationCore/Entities/AppEntities/Car.cs
@@ -1,17 +1,16 @@
-﻿using ApplicationCore.Enums;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using ApplicationCore.Enums;
 
 namespace ApplicationCore.Entities.AppEntities
 {
     public class Car : BaseEntity
     {
-        public string Brand { get; set; }
-        public string Model { get; set; }
+        public string CarBrand { get; set; }
+        public string CarType { get; set; }
+        public string CarColor { get; set; }
         public int ProductionYear { get; set; }
-        public Color Color { get; set; }
         public string RegistrationCertificate { get; set; }
         public string LicensePlate { get; set; }
-        public int CarTypeId { get; set; }
-        public CarType CarType { get; set; }
         public Driver Driver { get; set; }
         public bool IsDeleted { get; set; }
     }
