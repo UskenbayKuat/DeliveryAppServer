f4183a0af211809e90e6a2b5bcf8e81763eb0db6
Yerdaulet Bulatov <yerdaulet.bulatov@gmail.com>
2022-09-29 01:43:34 +0600

    #4 Обновлены классы CreateCar.CreateCarCommand и CreateCarInfo

--- src/PublicApi/Endpoints/Drivers/CreateCar/CreateCar.CreateCarCommand.cs
+++ src/PublicApi/Endpoints/Drivers/CreateCar/CreateCar.CreateCarCommand.cs
@@ -4,13 +4,12 @@ namespace PublicApi.Endpoints.Drivers.CreateCar
 {
     public class CreateCarCommand
     {
-        public string Brand { get; set; }
-        public string Model { get; set; }
+        public string CarBrand { get; set; }
+        public string CarType { get; set; }
+        public string CarColor { get; set; }
         public int ProductionYear { get; set; }
-        public Color Color { get; set; }
         public string RegistrationCertificate { get; set; }
         public string LicensePlate { get; set; }
-        public int CarTypeId { get; set; }
-        public bool IsDeleted { get; set; } = false;
+        public bool IsDeleted { get; set; }
     }
 }
