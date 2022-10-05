5f2e2b455fe454756734da9fabe987050fd9a165
yerdauletbulatov <107347697+yerdauletbulatov@users.noreply.github.com>
2022-10-05 16:52:59 +0600

    #11 Созданы сущности DriverAppDataInfo и ClientAppDataInfo

---
+++ src/ApplicationCore/Entities/ApiEntities/ClientAppDataInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ApplicationCore.Entities.AppEntities;
+
+namespace ApplicationCore.Entities.ApiEntities
+{
+    public class ClientAppDataInfo
+    {
+        public List<City> Cities { get; set; }
+        public List<CarType> CarTypes { get; set; }
+    }
+}
