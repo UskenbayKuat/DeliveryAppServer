bb048b9fe06917c031eded17a08354465913b7fb
yerdauletbulatov <107347697+yerdauletbulatov@users.noreply.github.com>
2022-10-05 16:53:54 +0600

    #11 Созданы endpoint-ы DriverAppData и ClientAppData

---
+++ src/PublicApi/Endpoints/Shared/ClientAppData/ClientAppData.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ApplicationCore.Entities.ApiEntities;
+using ApplicationCore.Interfaces.SharedInterfaces;
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PublicApi.Endpoints.Shared.ClientAppData
+{
+    public class ClientAppData : EndpointBaseAsync.WithoutRequest.WithActionResult
+    {
+        private readonly IDeliveryAppData<ClientAppDataInfo> _deliveryAppData;
+
+        public ClientAppData(IDeliveryAppData<ClientAppDataInfo> deliveryAppData)
+        {
+            _deliveryAppData = deliveryAppData;
+        }
+
+        [HttpGet("api/ClientAppData")]
+        public override Task<ActionResult> HandleAsync(CancellationToken cancellationToken = new CancellationToken()) 
+            => _deliveryAppData.SendData(cancellationToken);
+    }
+}
