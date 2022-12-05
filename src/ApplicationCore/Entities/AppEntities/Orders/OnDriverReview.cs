dc244977b5a7a31c8c86560d2c2665f25acb7a05
yerdauletbulatov <107347697+yerdauletbulatov@users.noreply.github.com>
2022-12-06 03:28:00 +0600

    #24 Создана новая таблица "На рассмотрении у водителя"

---
+++ src/ApplicationCore/Entities/AppEntities/Orders/OnDriverReview.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationCore.Entities.AppEntities.Orders
+{
+    public class OnDriverReview : BaseEntity
+    {
+        public OnDriverReview()
+        {
+            OnReview = true;
+        }
+        public bool OnReview { get; set;}
+        public RouteTrip RouteTrip { get; set;}
+    }
+}
